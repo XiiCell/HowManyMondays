c6ff4e6898f56a5d50aa6599a7923286380c800c
XiiCell <shawn.schulze+Git@gmail.com>
2014-06-14 16:39:15 -0400

    Fixed Days of Month Count
    Error with certain DS and DE amounts, revised code.

--- howManyMondays.js
+++ howManyMondays.js
@@ -3,17 +3,20 @@
  */
 
 function howManyMondays() {
-var x = 0,
-    m = 1 + parseInt($("#ui-datepicker-div .ui-datepicker-month :selected").val(),10) || 5,
-    y = $("#ui-datepicker-div .ui-datepicker-year :selected").val() || 2014,
-    html="",
-    ds = new Date(y, m - 1, 1).getDay(),
-    de = new Date(y, m, 0).getDay(),
-    dz = [' Sun', ' Mon', ' Tue', ' Wednes', ' Thurs', ' Fri', ' Satur'];
+    var dg = function (id) {
+            return document.getElementById(id);
+        },
+        x = 0,
+        m = 1 + parseInt($("#ui-datepicker-div .ui-datepicker-month :selected").val(), 10) || 1,
+        y = $("#ui-datepicker-div .ui-datepicker-year :selected").val() || 2014,
+        dda = new Date(y, m - 1, 29),
+        dz = ['4 Sun', '4 Mon', '4 Tue', '4 Wednes', '4 Thurs', '4 Fri', '4 Satur'];
 
-for (; x <= 6; x++) {
-    html+="There are " + (x >=ds & x<=de & (ds+6 !== de) ? "5" : "4") + dz[x] + "days in " + m + "/" + y + "<br/>"   ;
+    for (; dda.getMonth() === m - 1; dda.setDate(dda.getDate() + 1)) {
+        dz[dda.getDay()] = 5 + dz[dda.getDay()].substr(1, 10);
+    }
+
+    for (; x <= 6; x++) {
+        dg("out").innerHTML += "There are " + dz[x] + "days in " + m + "/" + y + "<br/>";
+    }
 }
-
-document.getElementById("out").innerHTML=html;
-};
